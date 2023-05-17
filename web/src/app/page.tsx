dca6697d49544822718d7f5a6a0714e0485fc222
Gabriely Rodrigues <gabyrlima.99@gmail.com>
2023-05-16 21:42:25 -0300

    web: setting fonts

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
       <h3 className="text-4xl font-bold">Sua cápsula do tempo</h3>
+      <h3 className="font-alt text-4xl">Sua cápsula do tempo</h3>
     </div>
-  );
+  )
 }
