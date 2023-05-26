8cd9a99747d00bc099b61545ee6fcd4e744bc4d3
Gabriely Rodrigues <gabyrlima.99@gmail.com>
2023-05-25 22:38:28 -0300

    mobile: new memory page

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -54,6 +54,7 @@ export default function Layout() {
         }}
       >
         <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="new" />
         <Stack.Screen name="memories" />
       </Stack>
     </ImageBackground>
